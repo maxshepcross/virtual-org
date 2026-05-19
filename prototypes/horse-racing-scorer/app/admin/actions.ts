fec50d6669bef5706f3b4eaa16687ed234c80e73
Max Shepherd-Cross <max@getofficely.com>
2026-05-19 11:33:46 +1000

    chore: remove extracted 15 to follow prototype

--- prototypes/horse-racing-scorer/app/admin/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { redirect } from "next/navigation";
-import { signOutAdmin } from "@/lib/auth";
-
-export async function signOutAction() {
-  await signOutAdmin();
-  redirect("/admin/login");
-}
